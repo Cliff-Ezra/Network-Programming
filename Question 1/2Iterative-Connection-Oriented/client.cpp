125900c35f50a669d8b054a8609c874693907e0c
Teddy Mwai <teddymwai11@gmail.com>
2023-05-05 06:17:58 +0300

    fixed duplicate bugs

--- Question 1/2Iterative-Connection-Oriented/client.cpp
+++ Question 1/2Iterative-Connection-Oriented/client.cpp
@@ -5,21 +5,27 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+int readSerialNumber(int client_socket);
+int readRegistrationNumber(int client_socket);
+
 // Serial Number Program
-int main() {
+int main()
+{
     // Create a socket and connect to the server
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
+    if (client_socket == -1)
+    {
         std::cerr << "Failed to create client socket\n";
         return 1;
     }
 
     sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(3000);
+    server_address.sin_port = htons(4000);
     server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if (connect(client_socket, (sockaddr*)&server_address, sizeof(server_address)) == -1) {
+    if (connect(client_socket, (sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
         std::cerr << "Failed to connect to server\n";
         return 1;
     }
@@ -31,79 +37,44 @@ int main() {
     std::string name;
 
     // Loop until the user chooses to exit
-    while (true) {
-        std::cout << "Enter a serial number (or 'exit' to quit): ";
-        std::getline(std::cin, serial_number);
-
-        // Check if the user wants to exit
-        if (serial_number == "exit") {
-            send(client_socket, serial_number.c_str(), serial_number.size(), 0);
+    while (true)
+    {
+        // Read the serial number from the user
+        int serial_number = readSerialNumber(client_socket);
+        // check if there was an error
+        if (serial_number == 1)
+        {
+            std::cerr << "Failed to read serial number\n";
             break;
         }
 
-        // Send the serial number to the server
-        int bytes_sent = send(client_socket, serial_number.c_str(), serial_number.size(), 0);
-        if (bytes_sent == -1) {
-            std::cerr << "Failed to send data to server\n";
-            continue;
+        // Read the registration number from the user
+        int registration_number = readRegistrationNumber(client_socket);
+        // check if there was an error
+        if (registration_number == 1)
+        {
+            std::cerr << "Failed to read registration number\n";
+            break;
         }
-
-        // Wait for response from server
+        int bytes_sent, bytes_received;
         char buffer[1024];
-        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-        if (bytes_received == -1) {
-            std::cerr << "Failed to receive data from server\n";
-            continue;
-        }
-
-        std::string response(buffer, bytes_received);
-        std::cout << response << std::endl;
-
-        if (response == "Serial number already exists.\n") {
-            continue;
-        }
-
-        while (true) {
-            std::cout << "Enter a Registration Number (P15/...): ";
-            std::getline(std::cin, registration_number);
-
-            // Send the registration number to the server
-            bytes_sent = send(client_socket, registration_number.c_str(), registration_number.size(), 0);
-            if (bytes_sent == -1) {
-                std::cerr << "Failed to send data to server\n";
-                break;
-            }
-
-            // Wait for response from server
-            bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-            if (bytes_received == -1) {
-                std::cerr << "Failed to receive data from server\n";
-                break;
-            }
-
-            response = std::string(buffer, bytes_received);
-            std::cout << response << std::endl;
-
-            if (response == "Registration number already exists.\n") {
-                continue;
-            } else {
-                break;
-            }
-        }
+        std::string response;
 
         std::cout << "Enter a name: ";
         std::getline(std::cin, name);
 
         // Send the name to the server
         bytes_sent = send(client_socket, name.c_str(), name.size(), 0);
-        if (bytes_sent == -1) {
+        if (bytes_sent == -1)
+        {
             std::cerr << "Failed to send data to server\n";
             continue;
         }
 
         // Wait for response from server
         bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-        if (bytes_received == -1) {
+        if (bytes_received == -1)
+        {
             std::cerr << "Failed to receive data from server\n";
             continue;
         }
@@ -119,4 +90,76 @@ int main() {
 }
 
 // Server path:
-// 
+// function to read the serail number from the client
+int readSerialNumber(int client_socket)
+{
+    std::string serial_number;
+    std::cout << "Enter a serial number (or 'exit' to quit): ";
+    std::getline(std::cin, serial_number);
+
+    // Check if the user wants to exit
+    if (serial_number == "exit")
+    {
+        send(client_socket, serial_number.c_str(), serial_number.size(), 0);
+        return 0;
+    }
+
+    // Send the serial number to the server
+    int bytes_sent = send(client_socket, serial_number.c_str(), serial_number.size(), 0);
+    if (bytes_sent == -1)
+    {
+        std::cerr << "Failed to send data to server\n";
+        return 1;
+    }
+
+    // Wait for response from server
+    char buffer[1024];
+    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+    if (bytes_received == -1)
+    {
+        std::cerr << "Failed to receive data from server\n";
+        return 1;
+    }
+
+    std::string response(buffer, bytes_received);
+    std::cout << response << std::endl;
+    if (response == "Serial number already exists.")
+    {
+        std::cout << "Please enter a different serial number." << std::endl;
+        readSerialNumber(client_socket);
+    }
+    return 0;
+}
+
+int readRegistrationNumber(int client_socket)
+{
+    std::string registration_number;
+    std::cout << "Enter a Registration Number (P15/...): ";
+    std::getline(std::cin, registration_number);
+
+    // Send the registration number to the server
+    int bytes_sent = send(client_socket, registration_number.c_str(), registration_number.size(), 0);
+    if (bytes_sent == -1)
+    {
+        std::cerr << "Failed to send data to server\n";
+        return 1;
+    }
+
+    // Wait for response from server
+    char buffer[1024];
+    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+    if (bytes_received == -1)
+    {
+        std::cerr << "Failed to receive data from server\n";
+        return 1;
+    }
+
+    std::string response(buffer, bytes_received);
+    std::cout << response << std::endl;
+    if (response == "Registration number already exists")
+    {
+        std::cout << "Please enter a different registration number." << std::endl;
+        readRegistrationNumber(client_socket);
+    }
+    return 0;
+}
