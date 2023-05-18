79b45bd101ffe623539edfb479b94ac651c4607f
Teddy Mwai <teddymwai11@gmail.com>
2023-05-18 22:34:30 +0300

    added code for rpc calculator✅. rpc registration🪲

--- Question 4/Registration/client.c
+++ Question 4/Registration/client.c
@@ -1,10 +1,3 @@
-static char ibmcopyr[] =
-    "GENESEND - Licensed Materials - Property of IBM. "
-    "This module is \"Restricted Materials of IBM\" "
-    "5647-A01 Copyright IBM Corp. 2013. "
-    "See IBM Copyright Instructions.";
-
-#define MVS
 #include <stdio.h>
 #include <rpc/rpc.h>
 #include <sys/socket.h>
@@ -13,34 +6,92 @@ static char ibmcopyr[] =
 #define version ((u_long)1)
 #define intrcvproc ((u_long)1)
 
-main(argc, argv) int argc;
-char *argv[];
+bool_t xdr_input_data(
+    XDR *xdrs, struct {
+        int serial_number;
+        int registration_number;
+        char student_name[256];
+    } * data)
 {
-    int innumber;
-    int outnumber;
-    int error;
-
-    if (argc != 3)
+    if (!xdr_int(xdrs, &data->serial_number))
     {
-        fprintf(stderr, "usage: %s hostname integer\n", argv[0]);
-        exit(-1);
-    } /* endif */
-    innumber = atoi(argv[2]);
-    /*
-     * Send the integer to the server. The server should
-     * return the same integer.
-     */
-    error = callrpc(argv[1], intrcvprog, version, intrcvproc, xdr_int,
-                    (char *)&innumber, xdr_int, (char *)&outnumber);
+        return FALSE;
+    }
+    if (!xdr_int(xdrs, &data->registration_number))
+    {
+        return FALSE;
+    }
+    if (!xdr_string(xdrs, &data->student_name, 256))
+    {
+        return FALSE;
+    }
+    return TRUE;
+}
 
+int main(int argc, char *argv[])
+{
+    int serial_number;
+    int registration_number;
+    int error;
+    int result;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s hostname\n", argv[0]);
+        exit(-1);
+    }
+
+    printf("Enter Serial Number: ");
+    scanf("%d", &serial_number);
+
+    printf("Enter Student Registration Number: ");
+    scanf("%d", &registration_number);
+
+    char *student_name = malloc(256 * sizeof(char));
+    if (student_name == NULL)
+    {
+        fprintf(stderr, "Error: Could not allocate memory for student name\n");
+        exit(1);
+    }
+
+    printf("Enter Student Name: ");
+    scanf("%s", student_name);
+    // append null character to the end of the string
+    student_name[255] = '\0';
+
+    struct
+    {
+        int serial_number;
+        int registration_number;
+        char student_name[256];
+    } indata = {serial_number, registration_number};
+
+    strcpy(indata.student_name, student_name);
+
+    error = callrpc(argv[1], intrcvprog, version, intrcvproc, xdr_input_data, (char *)&indata, xdr_int, (char *)&result);
     if (error != 0)
     {
         fprintf(stderr, "error: callrpc failed: %d \n", error);
-        fprintf(stderr, "intrcprog: %d version: %d intrcvproc: %d",
-                intrcvprog, version, intrcvproc);
+        fprintf(stderr, "intrcprog: %d version: %d intrcvproc: %d", intrcvprog, version, intrcvproc);
         exit(1);
-    } /* endif */
+    }
 
-    printf("value sent: %d   value received: %d\n", innumber, outnumber);
+    if (result == 0)
+    {
+        printf("Student details saved successfully\n");
+    }
+    else if (result == 1)
+    {
+        printf("Error: Serial number already exists\n");
+    }
+    else if (result == 2)
+    {
+        printf("Error: Registration number already exists\n");
+    }
+    else if (result == 3)
+    {
+        printf("Error: Both serial number and registration number already exist\n");
+    }
+
     exit(0);
 }
--- Question 4/Registration/server.c
+++ Question 4/Registration/server.c
@@ -1,14 +1,10 @@
-static char ibmcopyr[] =
-    "GENESERV - Licensed Materials - Property of IBM. "
-    "This module is \"Restricted Materials of IBM\" "
-    "5647-A01  Copyright IBM Corp. 2013. "
-    "See IBM Copyright Instructions.";
-
 #ifndef MVS
 #define MVS
 #endif
+
 #include <rpc/rpc.h>
 #include <stdio.h>
+#include <string.h>
 
 #define intrcvprog ((u_long)150000)
 #define fltrcvprog ((u_long)150102)
@@ -17,56 +13,96 @@ static char ibmcopyr[] =
 #define fltrcvproc ((u_long)1)
 #define fltvers ((u_long)1)
 
-main()
+bool_t xdr_input_data(
+    XDR *xdrs, struct {
+        int serial_number;
+        int registration_number;
+        char student_name[256];
+    } * data)
+{
+    if (!xdr_int(xdrs, &data->serial_number))
+    {
+        return FALSE;
+    }
+    if (!xdr_int(xdrs, &data->registration_number))
+    {
+        return FALSE;
+    }
+    if (!xdr_string(xdrs, &data->student_name, 256))
+    {
+        return FALSE;
+    }
+    return TRUE;
+}
+
+int main()
 {
     int *intrcv();
-    float *floatrcv();
 
-    /*REGISTER PROG, VERS AND PROC WITH THE PORTMAPPER*/
-
-    /*FIRST PROGRAM*/
-    registerrpc(intrcvprog, intvers, intrcvproc, intrcv, xdr_int, xdr_int);
+    registerrpc(intrcvprog, intvers, intrcvproc, intrcv, xdr_input_data, xdr_int);
     printf("Intrcv Registration with Port Mapper completed\n");
 
-    /*OR MULTIPLE PROGRAMS*/
-    registerrpc(fltrcvprog, fltvers, fltrcvproc,
-                floatrcv, xdr_float, xdr_float);
-    printf("Floatrcv Registration with Port Mapper completed\n");
-
-    /*
-     * svc_run will handle all requests for programs registered.
-     */
     svc_run();
     printf("Error:svc_run returned!\n");
     exit(1);
 }
 
-/*
- * Procedure called by the server to receive and return an integer.
- */
-int *intrcv(in)
-int *in;
+int *intrcv(struct {
+    int serial_number;
+    int registration_number;
+    char student_name[256];
+} * in)
 {
-    int *out;
+    static int result;
 
-    printf("integer received: %d\n", *in);
-    out = in;
-    printf("integer being returned: %d\n", *out);
-    return (out);
-}
+    FILE *file = fopen("Students.txt", "a+");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Could not open file\n");
+        exit(1);
+    }
 
-/*
- * Procedure called by the server to receive and return a float.
- */
+    int existing_serial_number;
+    int existing_registration_number;
+    char existing_student_name[256];
+    int serial_number_exists = 0;
+    int registration_number_exists = 0;
 
-float *
-floatrcv(in)
-float *in;
-{
-    float *out;
+    while (fscanf(file, "%d %d %s\n", &existing_serial_number, &existing_registration_number, existing_student_name) != EOF)
+    {
+        if (existing_serial_number == in->serial_number)
+        {
+            serial_number_exists = 1;
+        }
+        if (existing_registration_number == in->registration_number)
+        {
+            registration_number_exists = 1;
+        }
+        if (serial_number_exists && registration_number_exists)
+        {
+            break;
+        }
+    }
 
-    printf("float received: %e\n", *in);
-    out = in;
-    printf("float being returned: %e\n", *out);
-    return (out);
+    if (serial_number_exists && registration_number_exists)
+    {
+        result = 3;
+    }
+    else if (serial_number_exists)
+    {
+        result = 1;
+    }
+    else if (registration_number_exists)
+    {
+        result = 2;
+    }
+    else
+    {
+        fprintf(file, "%d %d %s\n", in->serial_number, in->registration_number, in->student_name);
+        result = 0;
+    }
+
+    fclose(file);
+
+    return &result;
 }
